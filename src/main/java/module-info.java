06d1a32402f9f2b2cb78eb323278d329b67d4fbe
许轲 <luke.k.xu@hotmail.com>
2023-08-12 11:50:13 +0800

    重构代码结构

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,8 @@ module org.jcnc.jnotepad {
     requires javafx.controls;
 
     exports org.jcnc.jnotepad;
-    exports org.jcnc.jnotepad.controller;
     exports org.jcnc.jnotepad.tool;
     exports org.jcnc.jnotepad.Interface;
-    exports org.jcnc.jnotepad.view;
     exports org.jcnc.jnotepad.controller.event.handler;
     exports org.jcnc.jnotepad.controller.manager;
     exports org.jcnc.jnotepad.view.init;
