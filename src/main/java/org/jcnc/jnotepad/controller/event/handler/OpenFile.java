704e928bb4029d5a60778a4b6dac9349eaec6fc8
许轲 <luke.k.xu@hotmail.com>
2023-08-14 12:41:35 +0800

    OpenFile.java 增加注释

--- src/main/java/org/jcnc/jnotepad/controller/event/handler/OpenFile.java
+++ src/main/java/org/jcnc/jnotepad/controller/event/handler/OpenFile.java
@@ -11,31 +11,47 @@ import java.io.File;
 
 import static org.jcnc.jnotepad.view.manager.ViewManager.tabPane;
 
-// 打开文件事件处理器
+/**
+ * 打开文件事件处理器.
+ */
 public class OpenFile implements EventHandler<ActionEvent> {
+    /**
+     * 处理打开文件事件.
+     *
+     * @param event 事件对象
+     */
     @Override
     public void handle(ActionEvent event) {
+        // 获取控制器
         Controller controller = new Controller();
+        // 创建文件选择器
         FileChooser fileChooser = new FileChooser();
+        // 显示文件选择对话框，并获取选中的文件
         File file = fileChooser.showOpenDialog(null);
         if (file != null) {
+            // 创建打开文件的任务
             Task<Void> openFileTask = new Task<>() {
                 @Override
                 protected Void call() {
+                    // 调用控制器的getText方法，读取文件内容
                     controller.getText(file);
+                    // 更新编码标签
                     controller.upDateEncodingLabel(((TextArea) tabPane.getSelectionModel().getSelectedItem().getContent()).getText());
                     return null;
                 }
             };
 
+            // 设置任务成功完成时的处理逻辑
             openFileTask.setOnSucceeded(e -> {
-                // 在需要时处理成功
+                // 处理成功的逻辑
             });
 
+            // 设置任务失败时的处理逻辑
             openFileTask.setOnFailed(e -> {
-                // 在需要时处理失败
+                // 处理失败的逻辑
             });
 
+            // 创建并启动线程执行任务
             Thread thread = new Thread(openFileTask);
             thread.start();
         }
