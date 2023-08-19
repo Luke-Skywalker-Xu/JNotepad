87ce23ce122860dcb3d046697403c5d0bcda054c
songdragon <songdragon@zts.io>
2023-08-19 16:04:00 +0800

    feat: 行号宽度根据最后一个行号自动调整

--- src/main/java/org/jcnc/jnotepad/ui/LineNumberTextArea.java
+++ src/main/java/org/jcnc/jnotepad/ui/LineNumberTextArea.java
@@ -13,16 +13,18 @@ public class LineNumberTextArea extends BorderPane {
     private final TextArea mainTextArea;
     private final TextArea lineNumberArea;
 
+    static final int[] sizeTable = {9, 99, 999, 9999, 99999, 999999, 9999999,
+            99999999, 999999999, Integer.MAX_VALUE};
+
     public LineNumberTextArea() {
         mainTextArea = new TextArea();
         lineNumberArea = new TextArea();
         lineNumberArea.setEditable(false);
-        lineNumberArea.setMinWidth(30);
-        lineNumberArea.setMaxWidth(30);
+        lineNumberArea.setPrefWidth(30);
         // 设置显示滚动条样式类
         lineNumberArea.getStyleClass().add("text-line-number");
         /*mainTextArea.getStyleClass().add("show-scrollbars"); */
-
+        lineNumberArea.textProperty().addListener((observable, oldValue, newValue) -> updateLineNumberWidth());
         mainTextArea.textProperty().addListener((observable, oldValue, newValue) -> updateLineNumberArea());
 
         // 当主要文本区域的垂直滚动位置发生变化时，使行号文本区域的滚动位置保持一致
@@ -37,6 +39,22 @@ public class LineNumberTextArea extends BorderPane {
 
         setCenter(mainTextArea);
         setLeft(lineNumberArea);
+    }
+
+    private void updateLineNumberWidth() {
+        int numOfLines = mainTextArea.getParagraphs().size();
+        int count = 1;
+        for (int i = 0; i < sizeTable.length; i++) {
+            if (numOfLines <= sizeTable[i]) {
+                count = i + 1;
+                break;
+            }
+        }
+        //单数字宽度10像素，4为padding=左3+右1
+        int actualWidth = count * 10 + 4;
+        if (actualWidth > lineNumberArea.getWidth()) {
+            lineNumberArea.setPrefWidth(actualWidth);
+        }
     }
 
     public StringProperty textProperty() {
