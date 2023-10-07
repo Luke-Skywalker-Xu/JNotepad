fc0eec29320491ff1ee06f94dcf43e4b0141c2be
gewuyou <1063891901@qq.com>
2023-10-07 09:26:06 +0800

    🔨 修改名称过时的API

--- src/main/java/org/jcnc/jnotepad/views/root/top/menubar/menu/RunTopMenu.java
+++ src/main/java/org/jcnc/jnotepad/views/root/top/menubar/menu/RunTopMenu.java
@@ -71,7 +71,7 @@ public class RunTopMenu extends AbstractTopMenu {
         // 获取TextCodeArea的文本内容
 
         CenterTab centerTab = CenterTabPaneManager.getInstance().getSelected();
-        String code = centerTab.getLineNumberTextArea().getText();
+        String code = centerTab.getTextCodeArea().getText();
 
         // TextCodeArea的当前文本内容
         System.out.println("TextCodeArea的当前文本内容：" + code);
