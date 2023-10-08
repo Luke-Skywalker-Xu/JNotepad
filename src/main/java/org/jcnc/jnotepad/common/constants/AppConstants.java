1870bf6b90702f9c971abe1f261c4eef4dd78e74
许轲 <luke.k.xu@hotmail.com>
2023-10-08 20:58:32 +0800

    版本更新常量

--- src/main/java/org/jcnc/jnotepad/common/constants/AppConstants.java
+++ src/main/java/org/jcnc/jnotepad/common/constants/AppConstants.java
@@ -13,7 +13,7 @@ public class AppConstants {
     /**
      * 版本号
      */
-    public static final String VERSION = "1.0.13";
+    public static final String VERSION = "1.0.14";
     /**
      * 作者
      */
