9c3473a16606ce614723d58c37e197f2371c5241
许轲 <luke.k.xu@hotmail.com>
2023-10-11 02:08:34 +0800

    增加 BuildPanel.java 的注释

--- src/main/java/org/jcnc/jnotepad/component/module/vbox/BuildPanel.java
+++ src/main/java/org/jcnc/jnotepad/component/module/vbox/BuildPanel.java
@@ -7,15 +7,27 @@ import org.jcnc.jnotepad.component.module.vbox.components.DebugBox;
 import org.jcnc.jnotepad.component.module.vbox.components.RunBox;
 
 /**
- * 底部Run,Debug,Cmd面板
+ * 底部运行、调试和命令终端面板。
  *
+ * <p>这个类实现了一个包含运行信息、调试信息和命令终端的底部面板。它是TabPane的子类，用于将这三个组件以选项卡的形式显示在底部面板上。</p>
  *
+ * <p>可以通过调用getInstance方法获取单例实例。</p>
+ *
+ * @see CmdTerminalBox
+ * @see RunBox
+ * @see DebugBox
+ * @see TabPane
  * @author cccqyu
  */
 public class BuildPanel extends TabPane {
 
     private static BuildPanel instance = null;
 
+    /**
+     * 获取BuildPanel的单例实例。
+     *
+     * @return BuildPanel的单例实例
+     */
     public static BuildPanel getInstance() {
 
         if (instance == null) {
@@ -33,26 +45,41 @@ public class BuildPanel extends TabPane {
         runBox = new RunBox();
         debugBox = new DebugBox();
 
-        Tab runTab = new Tab("运行",runBox);
+        Tab runTab = new Tab("运行", runBox);
         runTab.setClosable(false);
 
-        Tab buildTab = new Tab("构建", debugBox);
+        Tab buildTab = new Tab("调试", debugBox);
         buildTab.setClosable(false);
 
-        Tab cmdTab = new Tab("终端",cmdTerminalBox);
+        Tab cmdTab = new Tab("命令终端", cmdTerminalBox);
         cmdTab.setClosable(false);
-        this.getTabs().addAll(runTab,buildTab,cmdTab);
+        this.getTabs().addAll(runTab, buildTab, cmdTab);
     }
 
+    /**
+     * 获取命令终端组件。
+     *
+     * @return CmdTerminalBox对象
+     */
     public CmdTerminalBox getCmdTerminalBox() {
         return cmdTerminalBox;
     }
 
+    /**
+     * 获取运行信息组件。
+     *
+     * @return RunBox对象
+     */
     public RunBox getRunBox() {
         return runBox;
     }
 
-    public DebugBox getBuildBox() {
+    /**
+     * 获取调试信息组件。
+     *
+     * @return DebugBox对象
+     */
+    public DebugBox getDebugBox() {
         return debugBox;
     }
 }
