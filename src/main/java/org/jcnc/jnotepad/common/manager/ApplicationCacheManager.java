70dc245d1eef39482f7c6c45fadf2fe3e962d5b9
许轲 <luke.k.xu@hotmail.com>
2023-10-11 02:08:26 +0800

    增加 ApplicationCacheManager.java 的注释

--- src/main/java/org/jcnc/jnotepad/common/manager/ApplicationCacheManager.java
+++ src/main/java/org/jcnc/jnotepad/common/manager/ApplicationCacheManager.java
@@ -3,8 +3,11 @@ package org.jcnc.jnotepad.common.manager;
 import org.jcnc.jnotepad.api.core.manager.AbstractCacheManager;
 
 /**
- * 应用程序缓存管理类
- *
+ * 应用程序缓存管理类。
+ * <p>
+ * 该类用于管理应用程序中的缓存数据。
+ * </p>
+ * 
  * @author gewuyou
  */
 public class ApplicationCacheManager extends AbstractCacheManager {
@@ -15,10 +18,20 @@ public class ApplicationCacheManager extends AbstractCacheManager {
 
     }
 
+    /**
+     * 获取 ApplicationCacheManager 的实例。
+     *
+     * @return ApplicationCacheManager 实例
+     */
     public static ApplicationCacheManager getInstance() {
         return INSTANCE;
     }
 
+    /**
+     * 获取全局命名空间。
+     *
+     * @return 全局命名空间字符串
+     */
     @Override
     public String getGlobalNamespace() {
         return "jcnc";
