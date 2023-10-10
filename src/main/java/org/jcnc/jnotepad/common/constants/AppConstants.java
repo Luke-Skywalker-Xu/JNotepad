8cd73016c55ebe4de0d0d7efeddd004ec7b2416e
许轲 <luke.k.xu@hotmail.com>
2023-10-11 02:08:20 +0800

    增加 AppConstants.java 的注释

--- src/main/java/org/jcnc/jnotepad/common/constants/AppConstants.java
+++ src/main/java/org/jcnc/jnotepad/common/constants/AppConstants.java
@@ -7,7 +7,15 @@ import java.util.regex.Pattern;
 /**
  * 应用常量类，用于存放应用程序中的常量值。
  *
- * @author 许轲
+ * <p>
+ * 该类包含了应用程序的一些常用常量，如版本号、作者、软件名称、初始宽度和高度、Logo地址等。
+ * </p>
+ *
+ * <p>
+ * 还包括了与标签页相关的正则表达式模式，以及默认的属性和程序文件目录等常量。
+ * </p>
+ *
+ * @author luke
  */
 public class AppConstants {
     /**
@@ -37,6 +45,10 @@ public class AppConstants {
 
     /**
      * 默认标签页的正则
+     *
+     * <p>
+     * 用于匹配默认标签页名称的正则表达式，格式为"New File"后跟数字。
+     * </p>
      */
     public static final Pattern TABNAME_PATTERN = Pattern.compile("^" + Pattern.quote(UiResourceBundle.getContent(TextConstants.NEW_FILE)) + "\\d+$");
 
@@ -49,11 +61,9 @@ public class AppConstants {
      */
     public static final String PROGRAM_FILE_DIRECTORY = ".jnotepad";
 
-
     /**
      * 私有构造函数，防止该类被实例化。
      */
     private AppConstants() {
     }
-
 }
