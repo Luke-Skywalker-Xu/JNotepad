c9c6152df24f776f5cb8b440777b77059b5ce5c9
许轲 <luke.k.xu@hotmail.com>
2023-10-11 02:05:37 +0800

    增加 UserConfig.java 的注释

--- src/main/java/org/jcnc/jnotepad/app/config/UserConfig.java
+++ src/main/java/org/jcnc/jnotepad/app/config/UserConfig.java
@@ -8,7 +8,12 @@ import java.util.List;
 /**
  * 用户配置文件类
  *
- * @author 许轲
+ * <p>
+ * 此类用于存储用户的配置信息，包括语言设置、文本自动换行设置和快捷键配置。
+ * </p>
+ *
+ * @author luke
+ *
  */
 public class UserConfig {
 
@@ -17,27 +22,56 @@ public class UserConfig {
     private boolean textWrap;
     private List<ShortcutKey> shortcutKey;
 
-
+    /**
+     * 获取语言设置
+     *
+     * @return 语言设置
+     */
     public String getLanguage() {
         return language;
     }
 
+    /**
+     * 设置语言设置
+     *
+     * @param language 语言设置
+     */
     public void setLanguage(String language) {
         this.language = language;
     }
 
+    /**
+     * 获取快捷键配置列表
+     *
+     * @return 快捷键配置列表
+     */
     public List<ShortcutKey> getShortcutKey() {
         return shortcutKey;
     }
 
+    /**
+     * 设置快捷键配置列表
+     *
+     * @param shortcutKey 快捷键配置列表
+     */
     public void setShortcutKey(List<ShortcutKey> shortcutKey) {
         this.shortcutKey = shortcutKey;
     }
 
+    /**
+     * 获取文本自动换行设置
+     *
+     * @return 是否启用文本自动换行
+     */
     public boolean isTextWrap() {
         return textWrap;
     }
 
+    /**
+     * 设置文本自动换行设置
+     *
+     * @param textWrap 是否启用文本自动换行
+     */
     public void setTextWrap(boolean textWrap) {
         this.textWrap = textWrap;
     }
