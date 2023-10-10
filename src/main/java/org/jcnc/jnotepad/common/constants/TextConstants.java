b5fe6c84519a3613a32a43acaf481645b66fdb29
许轲 <luke.k.xu@hotmail.com>
2023-10-11 02:06:01 +0800

    增加 TextConstants.java 的注释

--- src/main/java/org/jcnc/jnotepad/common/constants/TextConstants.java
+++ src/main/java/org/jcnc/jnotepad/common/constants/TextConstants.java
@@ -1,55 +1,171 @@
 package org.jcnc.jnotepad.common.constants;
 
-
 /**
- * 文本常量，被多处使用的常量放到此处。如果只有一个class使用，在class中使用private static final声明。
+ * 文本常量类，包含多处使用的文本常量。
+ *
+ * <p>如果只有一个类使用常量，请在该类中使用 <code>private static final</code> 声明。</p>
  *
  * @author gewuyou
  */
 public class TextConstants {
 
+    /**
+     * 标题文本常量
+     */
     public static final String TITLE = "title";
+
+    /**
+     * 保存文本常量
+     */
     public static final String SAVE = "SAVE";
+
+    /**
+     * 文件文本常量
+     */
     public static final String FILE = "FILE";
 
+    /**
+     * 构建文本常量
+     */
     public static final String BUILD = "BUILD";
+
+    /**
+     * 终端文本常量
+     */
     public static final String TERMINAL = "TERMINAL";
 
+    /**
+     * 运行文本常量
+     */
     public static final String RUN = "RUN";
+
+    /**
+     * 调试文本常量
+     */
     public static final String DE_BUG = "DE_BUG";
+
+    /**
+     * 新建文本常量
+     */
     public static final String NEW = "NEW";
+
+    /**
+     * 打开文本常量
+     */
     public static final String OPEN = "OPEN";
-    public static final String OPEN_DIRECTORY  = "OPEN_DIRECTORY";
+
+    /**
+     * 打开目录文本常量
+     */
+    public static final String OPEN_DIRECTORY = "OPEN_DIRECTORY";
+
+    /**
+     * 另存为文本常量
+     */
     public static final String SAVE_AS = "SAVE_AS";
+
+    /**
+     * 重命名文本常量
+     */
     public static final String RENAME = "RENAME";
+
+    /**
+     * 设置文本常量
+     */
     public static final String SET = "SET";
+
+    /**
+     * 帮助文本常量
+     */
     public static final String HELP = "HELP";
+
+    /**
+     * 自动换行文本常量
+     */
     public static final String WORD_WRAP = "WORD_WRAP";
+
+    /**
+     * 插件文本常量
+     */
     public static final String PLUGIN = "PLUGIN";
+
+    /**
+     * 管理插件文本常量
+     */
     public static final String MANAGER_PLUGIN = "MANAGER_PLUGIN";
 
+    /**
+     * 关于文本常量
+     */
     public static final String ABOUT = "ABOUT";
 
-    public static final String DEVELOPER = "DEVELOPER";
-    public static final String STATISTICS = "STATISTICS";
-    public static final String OPEN_CONFIGURATION_FILE = "OPEN_CONFIGURATION_FILE";
-    public static final String TOP = "TOP";
-    public static final String LANGUAGE = "LANGUAGE";
-    public static final String UPPER_CHINESE = "CHINESE";
-    public static final String UPPER_ENGLISH = "ENGLISH";
-    public static final String NEW_FILE = "NEW_FILE";
-    public static final String ROW = "ROW";
-    public static final String COLUMN = "COLUMN";
-    public static final String WORD_COUNT = "WORD_COUNT";
-    public static final String ENCODE = "ENCODE";
     /**
-     * 英文小写
+     * 开发者文本常量
+     */
+    public static final String DEVELOPER = "DEVELOPER";
+
+    /**
+     * 统计文本常量
+     */
+    public static final String STATISTICS = "STATISTICS";
+
+    /**
+     * 打开配置文件文本常量
+     */
+    public static final String OPEN_CONFIGURATION_FILE = "OPEN_CONFIGURATION_FILE";
+
+    /**
+     * 顶部文本常量
+     */
+    public static final String TOP = "TOP";
+
+    /**
+     * 语言文本常量
+     */
+    public static final String LANGUAGE = "LANGUAGE";
+
+    /**
+     * 中文文本常量
+     */
+    public static final String UPPER_CHINESE = "CHINESE";
+
+    /**
+     * 英文文本常量
+     */
+    public static final String UPPER_ENGLISH = "ENGLISH";
+
+    /**
+     * 新建文件文本常量
+     */
+    public static final String NEW_FILE = "NEW_FILE";
+
+    /**
+     * 行文本常量
+     */
+    public static final String ROW = "ROW";
+
+    /**
+     * 列文本常量
+     */
+    public static final String COLUMN = "COLUMN";
+
+    /**
+     * 字数统计文本常量
+     */
+    public static final String WORD_COUNT = "WORD_COUNT";
+
+    /**
+     * 编码文本常量
+     */
+    public static final String ENCODE = "ENCODE";
+
+    /**
+     * 英文小写文本常量
      */
     public static final String ENGLISH = "english";
 
-    /// BaseConfig 文本常量
     /**
-     * 中文小写
+     * 中文小写文本常量
      */
     public static final String CHINESE = "chinese";
 
