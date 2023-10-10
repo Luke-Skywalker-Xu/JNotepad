e469b8ba894583c45a1a593a40611a45aaf204a0
gewuyou <1063891901@qq.com>
2023-10-10 14:09:55 +0800

    🚩 完善文件图标支持

--- src/main/java/org/jcnc/jnotepad/model/entity/DirFileModel.java
+++ src/main/java/org/jcnc/jnotepad/model/entity/DirFileModel.java
@@ -1,7 +1,6 @@
 package org.jcnc.jnotepad.model.entity;
 
 import javafx.scene.Node;
-import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.util.List;
 
@@ -23,15 +22,6 @@ public class DirFileModel {
 
     private List<DirFileModel> childFile;
 
-
-    public DirFileModel(String path, String name, List<DirFileModel> childFile, FontIcon iconIsNotSelected, FontIcon iconIsSelected) {
-        this.path = path;
-        this.name = name;
-        this.childFile = childFile;
-        this.iconIsNotSelected = iconIsNotSelected;
-        this.iconIsSelected = iconIsSelected;
-    }
-
     public DirFileModel(String path, String name, List<DirFileModel> childFile, Node iconIsNotSelected, Node iconIsSelected) {
         this.path = path;
         this.name = name;
@@ -44,7 +34,7 @@ public class DirFileModel {
     public List<DirFileModel> getChildFile() {
         return childFile;
     }
-    
+
     public String getPath() {
         return path;
     }
@@ -70,17 +60,18 @@ public class DirFileModel {
         return iconIsNotSelected;
     }
 
-    public void setIconIsNotSelected(FontIcon iconIsNotSelected) {
+    public void setIconIsNotSelected(Node iconIsNotSelected) {
         this.iconIsNotSelected = iconIsNotSelected;
+    }
+
+    public void setIconIsSelected(Node iconIsSelected) {
+        this.iconIsSelected = iconIsSelected;
     }
 
     public Node getIconIsSelected() {
         return iconIsSelected;
     }
 
-    public void setIconIsSelected(FontIcon iconIsSelected) {
-        this.iconIsSelected = iconIsSelected;
-    }
 
     public void setChildFile(List<DirFileModel> childFile) {
         this.childFile = childFile;
