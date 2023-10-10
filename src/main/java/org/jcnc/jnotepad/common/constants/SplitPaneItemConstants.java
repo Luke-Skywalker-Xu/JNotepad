d4f02c84c7db0ddd159860367fc36d9695d02a04
许轲 <luke.k.xu@hotmail.com>
2023-10-11 02:06:07 +0800

    增加 SplitPaneItemConstants.java 的注释

--- src/main/java/org/jcnc/jnotepad/common/constants/SplitPaneItemConstants.java
+++ src/main/java/org/jcnc/jnotepad/common/constants/SplitPaneItemConstants.java
@@ -2,24 +2,31 @@ package org.jcnc.jnotepad.common.constants;
 
 /**
  * SplitPane常量类
- * *
+ *
  * <p>用于记录SplitPane中子组件的索引</p>
  *
  * @author cccqyu
  */
 public class SplitPaneItemConstants {
 
-    // rootSplitPane
-
-    // 上部
+    /**
+     * rootSplitPane中的上部分隔栏索引。
+     */
     public static final int ROOT_SPLIT_PANE_TOP_SPLIT_PANE = 0;
-    // 底部
-    public static final int ROOT_SPLIT_PANE_CMDBox = 1;
 
-    // rootSplitPane中的上部面板
-    // 左侧
+    /**
+     * rootSplitPane中的底部指令框索引。
+     */
+    public static final int ROOT_SPLIT_PANE_CMD_BOX = 1;
+
+    /**
+     * rootSplitPane中的上部面板的左侧索引。
+     */
     public static final int TOP_SPLIT_PANE_DIRECTORY_SIDEBAR_PANE = 0;
-    // 右侧
+
+    /**
+     * rootSplitPane中的上部面板的右侧索引。
+     */
     public static final int TOP_SPLIT_PANE_CENTER_TAB_PANE = 1;
 
 }
