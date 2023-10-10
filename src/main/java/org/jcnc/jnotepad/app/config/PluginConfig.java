0f8cc95e4014bc0387536511d0ccdfafa49d034f
许轲 <luke.k.xu@hotmail.com>
2023-10-11 02:06:25 +0800

    增加 PluginConfig.java 的注释

--- src/main/java/org/jcnc/jnotepad/app/config/PluginConfig.java
+++ src/main/java/org/jcnc/jnotepad/app/config/PluginConfig.java
@@ -7,15 +7,30 @@ import java.util.List;
 /**
  * 插件配置文件
  *
+ * <p>
+ * 此类用于存储插件的配置信息，包括插件描述符的列表。
+ * </p>
+ *
  * @author gewuyou
+ *
  */
 public class PluginConfig {
     private List<PluginDescriptor> plugins;
 
+    /**
+     * 获取插件描述符列表
+     *
+     * @return 插件描述符列表
+     */
     public List<PluginDescriptor> getPlugins() {
         return plugins;
     }
 
+    /**
+     * 设置插件描述符列表
+     *
+     * @param plugins 插件描述符列表
+     */
     public void setPlugins(List<PluginDescriptor> plugins) {
         this.plugins = plugins;
     }
